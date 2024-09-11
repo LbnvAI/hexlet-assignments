8e1b37411045dcbb4e5b4ad0ca60a0050a24d4fc
@hexlet/cli <support@hexlet.io>
2024-09-11 20:12:21 +0300

    submit java-oop-ru/reflections

---
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -0,0 +1,63 @@
+package exercise;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+// BEGIN
+public class Validator {
+    public static List<String> validate(Address address) {
+        List<String> invalid = new ArrayList<>();
+        for (Field field : address.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(NotNull.class)) {
+                try {
+                    field.setAccessible(true);
+                    Object value = field.get(address);
+                    if (Objects.isNull(value)) {
+                        var temp = field.toString().split("\\.");
+                        invalid.add(temp[temp.length - 1]);
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return invalid;
+    }
+
+    public static Map<String, List<String>> advancedValidate(Address address) {
+        Map<String, List<String>> errors = new HashMap<>();
+        for (Field field : address.getClass().getDeclaredFields()) {
+            List<String> errList = new ArrayList<>();
+            if (field.isAnnotationPresent(NotNull.class)) {
+                try {
+                    field.setAccessible(true);
+                    Object value = field.get(address);
+                    if (Objects.isNull(value)) {
+                        errList.add("can not be null");
+                    }
+                } catch (RuntimeException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (field.isAnnotationPresent(MinLength.class)) {
+                try {
+                    field.setAccessible(true);
+                    Object value = field.get(address);
+                    if (Objects.equals(value.getClass(), String.class)) {
+                        MinLength annotation = field.getAnnotation(MinLength.class);
+                        if (value.toString().length() < annotation.minLength()) {
+                            errList.add("length less than" + annotation.minLength());
+                        }
+                    }
+                } catch (RuntimeException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (!errList.isEmpty()) {
+                errors.put(field.getName(), errList);
+            }
+        }
+        return errors;
+    }
+}
+// END
